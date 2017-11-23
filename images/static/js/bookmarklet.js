e34e603339a9817462f043c7c4ed0116753b3864
doingandlearning <kev.cunningham@gmail.com>
2017-11-23 11:01:57 +0000

    allowed hosts

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,6 +1,6 @@
 (function() {
     var jquery_version = '2.1.4';
-    var site_url = 'http://kcshed.com:8000/';
+    var site_url = 'social-photo-bookmarking.herokuapp.com';
     var static_url = site_url + 'static/';
     var min_width = 100;
     var min_height = 100;
