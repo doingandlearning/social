9cb5f68fd468d5e660b4ea81c673b6c951be4376
doingandlearning <kev.cunningham@gmail.com>
2017-11-23 13:09:52 +0000

    changing functionality to link to img rather than copy

--- images/forms.py
+++ images/forms.py
@@ -21,19 +21,3 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
-
-    def save(self, force_insert=False, force_update=False, commit=True):
-        image = super(ImageCreateForm, self).save(commit=False)
-        image_url = self.cleaned_data['url']
-        image_name = '{}.{}'.format(slugify(image.title),
-                                    image_url.rsplit('.', 1)[1].lower())
-
-        # download image from the given URL
-        response = request.urlopen(image_url)
-        image.image.save(image_name,
-                         ContentFile(response.read()),
-                         save=False)
-
-        if commit:
-            image.save()
-        return image
